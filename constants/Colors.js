9d4b07bd1061f2dae9710d101ef4be43dc488b0c
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:41:31 +0800

    Update Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,10 @@
 export default {
-  WHITE : '#ffffff',
-  PURPLE : '#5a33e7',
-  GREEN : '#394c65',
-  BLUE : '#009eff',
-  HEAVYBLUE : '#00bada',
-  GREY : '#808080',
-  PINK : '#ff21ff',
-  DARK : '#111'
-}
+	WHITE: '#ffffff',
+	PURPLE: '#5a33e7',
+	GREEN: '#394c65',
+	BLUE: '#009eff',
+	HEAVYBLUE: '#00bada',
+	GREY: '#808080',
+	PINK: '#ff21ff',
+	DARK: '#111'
+};
