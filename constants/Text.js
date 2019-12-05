b307b76cb8a999f6c1cdb4e2d1a09bc3dbeacd33
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:41:50 +0800

    Update Text.js

--- constants/Text.js
+++ constants/Text.js
@@ -1,14 +1,12 @@
-import Colors from "./Colors";
+import Colors from './Colors';
 
 export const t = {
-    HeadText:{color:Colors.WHITE, fontSize:29.76},
-    TitleText:{color:Colors.GREEN, fontSize:17.94, marginTop:20, fontWeight:'600'},
-    ThinText:{color:Colors.GREY, fontSize:11, lineHeight:10 ,},
+	HeadText: { color: Colors.WHITE, fontSize: 29.76 },
+	TitleText: { color: Colors.GREEN, fontSize: 17.94, marginTop: 20, fontWeight: '600' },
+	ThinText: { color: Colors.GREY, fontSize: 11, lineHeight: 10 },
 
-
-    text_w: {color:Colors.WHITE, fontSize:12},
-    text_p: {color:Colors.PURPLE, fontSize:12},
-    text_g: {color:Colors.GREY, fontSize:12},
-    text_b: {color:Colors.BLUE, fontSize:12},
-
-}
+	text_w: { color: Colors.WHITE, fontSize: 12 },
+	text_p: { color: Colors.PURPLE, fontSize: 12 },
+	text_g: { color: Colors.GREY, fontSize: 12 },
+	text_b: { color: Colors.BLUE, fontSize: 12 }
+};
