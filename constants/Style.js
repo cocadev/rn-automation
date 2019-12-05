a1359238e13b15fa24dfe9bb75e9874976dfadcb
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:41:45 +0800

    Update Style.js

--- constants/Style.js
+++ constants/Style.js
@@ -1,22 +1,24 @@
-import Colors from "./Colors";
+import Colors from './Colors';
 
 export const i = {
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },input:{
-        borderColor: Colors.BLUE, 
-        borderWidth: 1, 
-        fontSize: 16, 
-        borderRadius: 4, 
-        padding:3 
-    }, button:{
-        backgroundColor:Colors.BLUE,
-        width:140,
-        alignItems:'center',
-        justifyContent:'center',
-        paddingHorizontal:20,
-        paddingVertical:5,
-        borderRadius:4
-      },
-}
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	input: {
+		borderColor: Colors.BLUE,
+		borderWidth: 1,
+		fontSize: 16,
+		borderRadius: 4,
+		padding: 3
+	},
+	button: {
+		backgroundColor: Colors.BLUE,
+		width: 140,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 20,
+		paddingVertical: 5,
+		borderRadius: 4
+	}
+};
