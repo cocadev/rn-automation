8aad8af0e56606428e134618a0473891d8166cf3
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:42:24 +0800

    Utils Service management

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 export default {
-    LOCAL_URL : 'http://192.168.1.39:3000/', // http:192.168.4.1/admin.html?masterkey=MASTER_KEY
-    LOCAL_PASSWORD : 'sii_',
-    LOCAL_PASSWORD_LENGTH : 4,
-    LOCAL_PING : 'https://io.siitec.com/apptest/aswer.json', // http:192.168.4.1/ping,
-    SERVER : 'io.siitec.com',
-    CONFIGURATION_URL : '', // http:192.168.4.1/siifirstconfig?admin_pass=XXXX&new_admin_pass
-  }
+	LOCAL_URL: 'http://192.168.1.39:3000/', // http:192.168.4.1/admin.html?masterkey=MASTER_KEY
+	LOCAL_PASSWORD: 'sii_',
+	LOCAL_PASSWORD_LENGTH: 4,
+	LOCAL_PING: 'https://io.siitec.com/apptest/aswer.json', // http:192.168.4.1/ping,
+	SERVER: 'io.siitec.com',
+	CONFIGURATION_URL: '' // http:192.168.4.1/siifirstconfig?admin_pass=XXXX&new_admin_pass
+};
--- utils/utils.js
+++ utils/utils.js
@@ -1,119 +1,133 @@
-import { AsyncStorage } from 'react-native';
 import { pic } from '../constants/Image';
 
 class UtilService {
+	////////////////////////
+	///// date systeme /////
+	////////////////////////
+	static getDateTime(date) {
+		let d = new Date(date);
+		const padWithZero = (number) => {
+			const string = number.toString();
+			if (number < 10) {
+				return '0' + string;
+			}
+			return string;
+		};
+		return padWithZero(d.getMonth() + 1) + '/' + padWithZero(d.getDate()) + '  ' + d.getFullYear();
+	}
 
-    ////////////////////////
-    ///// date systeme /////
-    ////////////////////////
-    static getDateTime(date) {
-        let d = new Date(date);
-        const padWithZero = number => {
-          const string = number.toString();
-          if (number < 10) {
-            return "0" + string;
-          }
-          return string;
-        };
-        return padWithZero(d.getMonth()+1)+'/'+padWithZero(d.getDate()) + '  ' + d.getFullYear()
-    }
+	static getHourMinutes(date) {
+		let dd = new Date(date);
+		let h = dd.getHours(),
+			m = dd.getMinutes();
+		let AP = ' AM';
+		if (h > 12) {
+			h = h - 12;
+			AP = ' PM';
+		}
 
-    static getHourMinutes(date){
-        let dd = new Date(date)
-        let h = dd.getHours(), m = dd.getMinutes()
-        let AP = ' AM'
-        if (h > 12){
-            h = h - 12;
-            AP = ' PM'
-        }
-        
-        return h+''+ AP
-    }
+		return h + '' + AP;
+	}
 
-    static getDay(date){
-        let dd = new Date(date)
-        let h = dd.getDay()
-        console.log('what is day ', h)
-        if (h == 0){
-            AP = ' Sunday '
-        }if (h == 1){
-            AP = ' Monday '
-        }if (h == 2){
-            AP = ' Tuesday '
-        }if (h == 3){
-            AP = ' Wednesday '
-        }if (h == 4){
-            AP = ' Thirsday '
-        }if (h == 5){
-            AP = ' Friday '
-        }if (h == 6){
-            AP = ' Saturday '
-        }
-        return AP
-    }
+	static getDay(date) {
+		let dd = new Date(date);
+		let h = dd.getDay();
+		console.log('what is day ', h);
+		if (h == 0) {
+			AP = ' Sunday ';
+		}
+		if (h == 1) {
+			AP = ' Monday ';
+		}
+		if (h == 2) {
+			AP = ' Tuesday ';
+		}
+		if (h == 3) {
+			AP = ' Wednesday ';
+		}
+		if (h == 4) {
+			AP = ' Thirsday ';
+		}
+		if (h == 5) {
+			AP = ' Friday ';
+		}
+		if (h == 6) {
+			AP = ' Saturday ';
+		}
+		return AP;
+	}
 
-    ////////////////////////
-    ///// date systeme /////
-    ////////////////////////
-    static getDateTime(date) {
-        let d = new Date(date);
-        const padWithZero = number => {
-          const string = number.toString();
-          if (number < 10) {
-            return "0" + string;
-          }
-          return string;
-        };
-        return padWithZero(d.getMonth()+1)+'/'+padWithZero(d.getDate()) + '  ' + d.getFullYear()
-    }
+	////////////////////////
+	///// date systeme /////
+	////////////////////////
+	static getDateTime(date) {
+		let d = new Date(date);
+		const padWithZero = (number) => {
+			const string = number.toString();
+			if (number < 10) {
+				return '0' + string;
+			}
+			return string;
+		};
+		return padWithZero(d.getMonth() + 1) + '/' + padWithZero(d.getDate()) + '  ' + d.getFullYear();
+	}
 
-    static getHourMinutes(date){
-        let dd = new Date(date)
-        let h = dd.getHours(), m = dd.getMinutes()
-        let AP = ' AM'
-        if (h > 12){
-            h = h - 12;
-            AP = ' PM'
-        }
-        
-        return h+''+ AP
-    }
+	static getHourMinutes(date) {
+		let dd = new Date(date);
+		let h = dd.getHours(),
+			m = dd.getMinutes();
+		let AP = ' AM';
+		if (h > 12) {
+			h = h - 12;
+			AP = ' PM';
+		}
 
-    static getDay(date){
-        let dd = new Date(date)
-        let h = dd.getDay()
-        console.log('what is day ', h)
-        if (h == 0){
-            AP = ' Sunday '
-        }if (h == 1){
-            AP = ' Monday '
-        }if (h == 2){
-            AP = ' Tuesday '
-        }if (h == 3){
-            AP = ' Wednesday '
-        }if (h == 4){
-            AP = ' Thirsday '
-        }if (h == 5){
-            AP = ' Friday '
-        }if (h == 6){
-            AP = ' Saturday '
-        }
-        return AP
-    }
+		return h + '' + AP;
+	}
 
-    static ChangeImg(h){
-        if (h == ''){
-            AP = pic.image_0
-        }
-        if (h == 1){
-            AP = pic.image_1
-        }if (h == 2){
-            AP = pic.image_2
-        }if (h == 3){
-            AP = pic.image_3
-        }
-        return AP
-    }
+	static getDay(date) {
+		let dd = new Date(date);
+		let h = dd.getDay();
+		console.log('what is day ', h);
+		if (h == 0) {
+			AP = ' Sunday ';
+		}
+		if (h == 1) {
+			AP = ' Monday ';
+		}
+		if (h == 2) {
+			AP = ' Tuesday ';
+		}
+		if (h == 3) {
+			AP = ' Wednesday ';
+		}
+		if (h == 4) {
+			AP = ' Thirsday ';
+		}
+		if (h == 5) {
+			AP = ' Friday ';
+		}
+		if (h == 6) {
+			AP = ' Saturday ';
+		}
+		return AP;
+	}
+
+	static ChangeImg(h) {
+		if (h == '') {
+			AP = pic.image_0;
+		}
+		if (h == 1) {
+			AP = pic.image_1;
+		}
+		if (h == 2) {
+			AP = pic.image_2;
+		}
+		if (h == 3) {
+			AP = pic.image_3;
+		}
+		return AP;
+	}
 }
 
-export default UtilService
+export default UtilService;
