7804ebe4fb1bef9e6703b66d22000e5fe1f1e847
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:41:01 +0800

    Update Box.js

--- components/Box.js
+++ components/Box.js
@@ -1,35 +1,30 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
-import { t } from '../constants/Text';
-import { Actions, Scene, Router } from 'react-native-router-flux'
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import Colors from '../constants/Colors';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const width = Dimensions.get('window').width;
 
 export default class Box extends React.Component {
- 
-  render() {
-    const { title } = this.props;
-    return (
-        <View onPress={()=>Actions.main()} style={styles.box}>
-            <Text style={{textAlign:'center', color:Colors.WHITE}}>{title}</Text>
-        </View>
-    )
-  }
+	render() {
+		const { title } = this.props;
+		return (
+			<View onPress={() => Actions.main()} style={styles.box}>
+				<Text style={{ textAlign: 'center', color: Colors.WHITE }}>{title}</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-     box:{
-        marginTop:12,
-        borderRadius:6,
-        width:width/4,
-        height:width/4.2,
-        marginRight:12,
-        backgroundColor:Colors.BLUE,
-        justifyContent:'center',
-        alignItems:'center',
-     },
-  });
-  
+	box: {
+		marginTop: 12,
+		borderRadius: 6,
+		width: width / 4,
+		height: width / 4.2,
+		marginRight: 12,
+		backgroundColor: Colors.BLUE,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
